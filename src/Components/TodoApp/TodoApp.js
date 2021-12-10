59a894b7a903c82e7acdd44d8d64c56b42de1476
miller2590 <miller2590@gmail.com>
2021-12-10 15:13:35 -0600

    created todo list and todo app

---
+++ src/Components/TodoApp/TodoApp.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import TodoList from "../TodoList/TodoList";
+import { Typography } from "@mui/material";
+import { Paper } from "@mui/material";
+import { AppBar } from "@mui/material";
+import { Toolbar } from "@mui/material";
+import { Grid } from "@mui/material";
+
+function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: "Clean Fishtank", completed: false },
+    { id: 2, task: "Wash Car", completed: true },
+    { id: 3, task: "Grow Beard", completed: false },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+  return (
+    <Paper elevation={3}>
+      <AppBar position="static" color="primary" enableColorOnDark>
+        <Toolbar>
+          <Typography color="inherit">Todos With Hooks</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoList todos={todos} />
+    </Paper>
+  );
+}
+
+export default TodoApp;
